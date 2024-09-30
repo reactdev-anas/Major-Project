6c2a828833048ca764507c7fbc92c34dd0d63f57
reactdev-anas <anas.reactdev@gmail.com>
2024-09-30 10:17:10 +0530

    Update Card-3.jsx

--- Frontend/src/Components/Projects/Card-3.jsx
+++ Frontend/src/Components/Projects/Card-3.jsx
@@ -35,12 +35,12 @@ function Card3() {
           initial={{ opacity: 0, x: -100 }}
           whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.5, delay: 0.5 }}
-          data-tooltip-id='my-tooltip-inline' data-tooltip-content=' E-commerce App '
+          data-tooltip-id='my-tooltip-inline' data-tooltip-content=' Cartify '
           className='project-3' >
           <div className="project-text-3">
             <TypographyMotion initial={{ opacity: 0, x: -100 }}
               whileInView={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.5, delay: 0.6 }} fontFamily='roboto' fontWeight='500' variant='h5'> E-commerce App </TypographyMotion>
+              transition={{ duration: 0.5, delay: 0.6 }} fontFamily='roboto' fontWeight='500' variant='h5'> Cartify </TypographyMotion>
             <TypographyMotion initial={{ opacity: 0, x: -10 }}
               whileInView={{ opacity: 1, x: 0 }}
               transition={{ duration: 0.5, delay: 0.7 }} fontFamily='roboto' marginTop={1} width='80%'   sx={{fontSize:{lg:'0.875rem', xs:'0.8rem'}}}>A React-based project eith a clean and responsive design, enabling users to easily add, remove, and manage items. feature real-time updates and a seamless shopping experience</TypographyMotion>
